2e1d6c4dd42e17ea1c5acc9055681a92c96ec5ca
kienau <oliverau93@gmail.com>
2024-01-25 00:03:30 -0500

    add promo banner

---
+++ storyblok/components/promo.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const prop = defineProps({
+        blok: {
+            type: Object,
+            default: () => ({}),
+        }
+    })
+</script>
+
+<template>
+    <div class="promo">
+        <div class="container">
+            <h2>{{ blok.Title }}</h2>
+            <div class="promo__text">
+                {{ blok.Text }}
+            </div>
+        </div>
+    </div>
+</template>
